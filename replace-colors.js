14eef43f62b34158dbaebd8296362f80638c5216
Umut Topuzoğlu <umut.topuzoglu@grob.de>
2017-07-01 14:25:14 +0200

    prepare color swap

--- replace-colors.js
+++ replace-colors.js
@@ -4,6 +4,6 @@ const colors = require('./colors.json');
 Object
   .keys(colors.dark)
   .forEach((name) => {
-    sh.sed('-i', colors.dark[name], colors.light[name], './bluloco-light.tmTheme')
-    sh.sed('-i', colors.dark[name], colors.light[name], './bluloco-light-color-theme.json')
+    // sh.sed('-i', colors.dark[name], colors.light[name], './themes/bluloco-light.tmTheme')
+    sh.sed('-i', colors.dark[name], colors.light[name], './themes/bluloco-light-color-theme.json')
   })
